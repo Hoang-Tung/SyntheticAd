d37645016cd5cb275fa0e39902b50609f67477d6
Hoang-Tung <tranhoangtung1992@gmail.com>
2015-04-20 08:24:15 +0700

    get and create maid

--- SyntheticAd/src/com/kenshin/SyntheticAd/dao/MaidObject.java
+++ SyntheticAd/src/com/kenshin/SyntheticAd/dao/MaidObject.java
@@ -1,17 +1,113 @@
 package com.kenshin.SyntheticAd.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+import com.kenshin.SyntheticAd.database.Database;
 import com.kenshin.SyntheticAd.dto.Maid;
 import com.kenshin.SyntheticAd.dto.Service;
 
 public class MaidObject extends ServiceObject {
 
-	public ArrayList<Maid> getByCategoryAndLocation(Connection connection,
-			String category_id, String location_id, String type, String offset) throws Exception{
-		ArrayList<Maid> datas = new ArrayList<Maid>();
+	public ArrayList<Maid> getMaidByCategoryAndLocation(Connection connection,
+			String category_id, String location_id, String type, String offset)
+			throws Exception {
+		ArrayList<Service> datas = new ArrayList<Service>();
+		ArrayList<Maid> maid_datas = new ArrayList<Maid>();
+
+		datas = this.getServiceByCategoryAndLocation(connection, category_id,
+				location_id, type, offset);
+
+		Database database = new Database();
+		Connection cont = database.getConnect();
 		
-		return datas;
+		
+		for (int i = 0; i < datas.size(); i++) {
+			Maid n_maid = new Maid();
+
+			n_maid.setId(datas.get(i).getId());
+			n_maid.setTitle(datas.get(i).getTitle());
+			n_maid.setDescription(datas.get(i).getDescription());
+			n_maid.setPrice(datas.get(i).getPrice());
+			n_maid.setCategory_id(datas.get(i).getCategory_id());
+			n_maid.setAddress(datas.get(i).getAddress());
+			n_maid.setPass(datas.get(i).getPass());
+			n_maid.setPhone_num(datas.get(i).getPhone_num());
+			n_maid.setLocation_id(datas.get(i).getLocation_id());
+			n_maid.setLat(datas.get(i).getLat());
+			n_maid.setLon(datas.get(i).getLon());
+			n_maid.setUpdated_at(datas.get(i).getUpdated_at());
+			n_maid.setCreated_at(datas.get(i).getCreated_at());
+			n_maid.setCondition(datas.get(i).getCondition());
+			n_maid.setType(datas.get(i).getType());
+			n_maid.setUser_id(datas.get(i).getUser_id());
+			n_maid.setCare_num(datas.get(i).getCare_num());
+			
+			try {
+				PreparedStatement ps = cont
+						.prepareStatement("SELECT * FROM testcraighslist.maid WHERE maid.post_id = ?;");
+				
+				ps.setLong(1, datas.get(i).getId());	
+				
+				ResultSet rs = ps.executeQuery();
+
+				while (rs.next()) {
+					n_maid.setMaid_id(rs.getString("id"));
+					n_maid.setArea(rs.getString("area"));
+					System.out.println(rs.getString("time_start"));
+					n_maid.setStart_time(rs.getString("time_start"));
+					System.out.println(rs.getString("time_end"));
+					n_maid.setEnd_time(rs.getString("time_end"));
+				}
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				throw e;
+			} 
+
+			maid_datas.add(n_maid);
+		}
+		
+		cont.close();
+		return maid_datas;
+	}
+
+	public Maid createMaid(Connection connection, Maid maid){
+		Maid n_Maid = new Maid();
+		Service p_Service = new Service();
+		
+		try {
+			p_Service = createService(connection, maid);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		Database database = new Database();
+		try {
+			Connection con = database.getConnect();
+			
+			PreparedStatement ps = con.prepareStatement("INSERT INTO testcraighslist.maid (`category_id`, `post_id`, `area`, `time_start`, `time_end`) VALUES (?, ?, ?, ?, ?);");
+			ps.setLong(1, maid.getCategory_id());
+			ps.setLong(2, p_Service.getId());
+			ps.setLong(3, (long) maid.getArea());
+			
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			
+			ps.setString(4, simpleDateFormat.format(maid.getStart_time()));
+			
+			ps.setString(5, simpleDateFormat.format(maid.getEnd_time()));
+			
+			ps.executeUpdate();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		return n_Maid;
 	}
 }
--- SyntheticAd/src/com/kenshin/SyntheticAd/dao/ServiceObject.java
+++ SyntheticAd/src/com/kenshin/SyntheticAd/dao/ServiceObject.java
@@ -7,17 +7,17 @@ import java.util.ArrayList;
 
 import com.kenshin.SyntheticAd.dto.Post;
 import com.kenshin.SyntheticAd.dto.Service;
-import com.kenshin.SyntheticAd.dto.Vehicle;
+
 
 public class ServiceObject {
-	
+
 	public ArrayList<Service> getAll(Connection connection) throws Exception {
 		ArrayList<Service> datas = new ArrayList<Service>();
 		try {
 			PreparedStatement ps = connection
 					.prepareStatement("SELECT * FROM testcraighslist.service");
 			ResultSet rs = ps.executeQuery();
-			
+
 			while (rs.next()) {
 				Service c = new Service();
 				c.setId(rs.getString("id"));
@@ -77,15 +77,15 @@ public class ServiceObject {
 
 				c.setUpdated_at(rs.getString("updated_at"));
 				System.out.println(rs.getString("updated_at"));
-				
+
 				if (rs.getString("pass") != null) {
 					c.setPass(rs.getString("pass"));
 				}
-				
+
 				c.setAddress(rs.getString("address"));
 				if (rs.getString("phone_num") != null)
 					c.setPhone_num(rs.getString("phone_num"));
-				
+
 				datas.add(c);
 			}
 			return datas;
@@ -99,11 +99,159 @@ public class ServiceObject {
 
 	}
 
-	public ArrayList<Service> getServiceByCategoryAndLocation(String location, String category_id, String offset){
+	public ArrayList<Service> getServiceByCategoryAndLocation(
+			Connection connection, String category_id, String location,
+			String type, String offset) throws Exception {
 		ArrayList<Service> datas = new ArrayList<Service>();
-		
-		
-		
-		return datas;
+
+		try {
+			PreparedStatement ps = connection
+					.prepareStatement("SELECT * FROM testcraighslist.service WHERE service.category_id = ? AND service.location_id = ? AND service.type = ? LIMIT 10 OFFSET ?;");
+			ps.setLong(1, Long.parseLong(category_id));
+			ps.setLong(2, Long.parseLong(location));
+			ps.setLong(3, Long.parseLong(type));
+			ps.setLong(4, Long.parseLong(offset));
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				Service c = new Service();
+				c.setId(rs.getString("id"));
+				System.out.println(rs.getString("id"));
+
+				c.setUser_id(rs.getString("user_id"));
+				System.out.println(rs.getString("user_id"));
+
+				c.setTitle(rs.getString("title"));
+				System.out.println(rs.getString("title"));
+
+				c.setCategory_id(rs.getString("category_id"));
+				System.out.println(rs.getString("category_id"));
+
+				c.setDescription(rs.getString("description"));
+				System.out.println(rs.getString("description"));
+
+				c.setType(rs.getString("type"));
+				System.out.println(rs.getString("type"));
+
+				c.setPrice(rs.getString("price"));
+				System.out.println(rs.getString("price"));
+
+				if (rs.getString("lon") != null) {
+					c.setLon(rs.getString("lon"));
+					System.out.println("lon " + rs.getString("lon"));
+				}
+
+				if (rs.getString("lat") != null) {
+					c.setLat(rs.getString("lat"));
+					System.out.println("lat " + rs.getString("lat"));
+				}
+				c.setLocation_id(rs.getString("location_id"));
+				System.out.println(rs.getString("location_id"));
+
+				if (rs.getString("image_url") != null) {
+					c.setImageUrl(rs.getString("image_url"));
+					System.out.println(rs.getString("image_url"));
+				}
+
+				if (rs.getString("condition") != null) {
+					c.setCondition(rs.getString("condition"));
+					System.out.println(rs.getString("condition"));
+				}
+
+				if (rs.getString("extend_type") != null) {
+					c.setExtend_type(rs.getString("extend_type"));
+				}
+
+				c.setCare_num(rs.getString("care_num"));
+
+				if (rs.getString("size") != null)
+					c.setSize(rs.getString("size"));
+
+				c.setCreated_at(rs.getString("created_at"));
+				System.out.println(rs.getString("created_at"));
+
+				c.setUpdated_at(rs.getString("updated_at"));
+				System.out.println(rs.getString("updated_at"));
+
+				if (rs.getString("pass") != null) {
+					c.setPass(rs.getString("pass"));
+				}
+
+				c.setAddress(rs.getString("address"));
+				if (rs.getString("phone_num") != null)
+					c.setPhone_num(rs.getString("phone_num"));
+
+				datas.add(c);
+			}
+			return datas;
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw e;
+		} finally {
+			if (connection != null)
+				connection.close();
+		}
 	}
+
+	public Service createService(Connection connection, Service service) throws Exception{
+		Service n_Service = new Service();
+		
+		try {
+			PreparedStatement ps = connection
+					.prepareStatement("INSERT INTO `testcraighslist`.`service` (`category_id`,"
+							+ " `title`, `description`, `price`, `condition`, `user_id`, `type`, `location_id`, `pass`, `image_url`, `address`, `phone_num`) "
+							+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			ps.setLong(1, service.getCategory_id());
+			ps.setString(2, service.getTitle());
+			ps.setString(3, service.getDescription());
+			ps.setLong(4, (long) service.getPrice());
+			ps.setLong(5, service.getCondition());
+			ps.setLong(6, service.getUser_id());
+			ps.setLong(7, service.getType());
+			ps.setLong(8, service.getLocation_id());
+			ps.setString(9, service.getPass());
+			ps.setString(10, service.getImageUrl());
+			ps.setString(11, service.getAddress());
+			if (service.getPhone_num() != null) {
+				ps.setString(12, service.getPhone_num());
+			} else {
+				ps.setString(12, "");
+			}
+
+			System.out.println(ps.executeUpdate());
+
+			try {
+				ResultSet result = ps.getGeneratedKeys();
+				
+				if (result.next()) {
+
+					PreparedStatement ps1 = connection
+							.prepareStatement("INSERT INTO `testcraighslist`.`title` (`category_id`, `post_id`, `title`)"
+									+ " VALUES (?, ?, ?);");
+					ps1.setLong(1, service.getCategory_id());
+					ps1.setLong(2, result.getLong(1));
+					n_Service.setId(result.getString(1));
+					ps1.setString(3, service.getTitle());
+					ps1.executeUpdate();
+					
+					
+				}
+
+			} catch (Exception e) {
+				// TODO: handle exception
+				throw e;
+			}
+			
+			return n_Service;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			if (connection != null)
+				connection.close();
+		}
+		
+		return n_Service;
+	}
+	
 }
